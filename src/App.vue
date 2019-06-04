f8802356f0363f44a878aeacec689ccee2b90fee
Ginger <ziqingnanyi@gmail.com>
2019-06-04 19:52:23 +0800

    build

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view />
+    {{ msg }}
   </div>
 </template>
 
@@ -11,6 +11,11 @@ import fastclick from 'fastclick'
 fastclick.attach(document.bod) // 解决移动端点击事件300毫秒延迟问题
 
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      msg: 'Hi! This is my Vue template'
+    }
+  }
 }
 </script>
